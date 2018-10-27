3fb4b94248b48c692a04303e512a8c38191b664f
myxc <70953073@qq.com>
2018-10-27 17:33:22 +0800

    create proj

---
+++ Classes/Person.h
@@ -0,0 +1,15 @@
+//
+//  Person.h
+//  PodDemo
+//
+//  Created by myxc on 2018/10/27.
+//  Copyright © 2018 myxc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+- (void)test;
+    
+@end
---
+++ Classes/Person.m
@@ -0,0 +1,17 @@
+//
+//  Person.m
+//  PodDemo
+//
+//  Created by myxc on 2018/10/27.
+//  Copyright © 2018 myxc. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+- (void)test {
+    NSLog(@"pod create test");
+}
+    
+@end
